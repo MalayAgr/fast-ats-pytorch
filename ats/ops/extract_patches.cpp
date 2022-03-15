692d00698b126e4aed14888d023021ac23f50bf4
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-03-15 23:29:52 +0530

    Make for loop parallel and add preliminary files for CUDA kernel

--- ats/ops/extract_patches.cpp
+++ ats/ops/extract_patches.cpp
@@ -4,7 +4,18 @@
 
 using namespace torch::indexing;
 
-torch::Tensor extract_patches(
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) \
+    CHECK_CUDA(x);     \
+    CHECK_CONTIGUOUS(x)
+
+torch::Tensor extract_patches_cuda(
+    torch::Tensor img,
+    torch::Tensor offsets,
+    c10::ArrayRef<long int> patch_size);
+
+torch::Tensor extract_patches_no_cuda(
     torch::Tensor img,
     torch::Tensor offsets,
     c10::ArrayRef<long int> patch_size)
@@ -23,22 +34,40 @@ torch::Tensor extract_patches(
 
     std::vector<torch::Tensor> patches;
 
-    for (int b = 0; b < img.size(0); b++)
-    {
-        for (int n = 0; n < offset_acc.size(1); n++)
+    at::parallel_for(0, img.size(0), 0, [&](int64_t start, int64_t end)
+                     {
+        for (int64_t b = start; b < end; b++)
         {
-            auto x_start = (int)(offset_acc[b][n][0]);
-            auto x_end = x_start + patch_H;
-            auto y_start = (int)(offset_acc[b][n][1]);
-            auto y_end = y_start + patch_W;
+            for (int64_t n = 0; n < offset_acc.size(1); n++)
+            {
+                auto x_start = (int)(offset_acc[b][n][0]);
+                auto x_end = x_start + patch_H;
+                auto y_start = (int)(offset_acc[b][n][1]);
+                auto y_end = y_start + patch_W;
 
-            auto patch = img.index({b, Slice(), Slice(x_start, x_end), Slice(y_start, y_end)});
+                auto patch = img.index({b, Slice(), Slice(x_start, x_end), Slice(y_start, y_end)});
 
-            patches.push_back(patch);
-        }
-    }
+                patches.push_back(patch);
+            }
+        } });
 
     return torch::stack(patches);
+}
+
+torch::Tensor extract_patches(
+    torch::Tensor img,
+    torch::Tensor offsets,
+    c10::ArrayRef<long int> patch_size)
+{
+    auto cuda_available = torch::cuda::is_available();
+
+    if (!cuda_available)
+        return extract_patches_no_cuda(img, offsets, patch_size);
+
+    // CHECK_INPUT(img);
+    // CHECK_INPUT(offsets);
+
+    // return extract_patches_cuda(img, offsets, patch_size);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
