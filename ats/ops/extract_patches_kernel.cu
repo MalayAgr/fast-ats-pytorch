21b661a04e7845c1cbb1e0b420114b2829095550
Ubuntu <malayagr@gpu-instance.wjsgot1ei3zuposwppvy5ufmmg.bx.internal.cloudapp.net>
2022-03-17 16:46:01 +0000

    Implement CUDA kernel

--- ats/ops/extract_patches_kernel.cu
+++ ats/ops/extract_patches_kernel.cu
@@ -9,7 +9,7 @@ template <typename scalar_t>
 __global__ void extract_patches_kernel(
     const torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> img,
     const torch::PackedTensorAccessor32<scalar_t, 3, torch::RestrictPtrTraits> offsets, const int64_t batch_size,
-    int64_t n_samples, int64_t channels, const int64_t patch_H, const int64_t patch_W,
+    const int64_t n_samples, const int64_t channels, const int64_t patch_H, const int64_t patch_W,
     torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> patches)
 {
     const int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -21,27 +21,31 @@ __global__ void extract_patches_kernel(
     if ((b >= batch_size) || (n >= n_samples) || (c >= channels))
         return;
 
-    auto patch_idx = (b + 1) * n;
+    const int patch_idx = (b * n_samples) + n;
 
-    printf("%l %l %l %l", b, n, c, patc_idx);
+    printf("%lu,%lu,%lu,%lu\n", b, n, c, patch_idx);
+    printf("%lu,%lu,%lu\n", batch_size, n_samples, channels);
+
 
     auto x_start = (int)(offsets[b][n][0]);
     auto x_end = x_start + patch_H;
     auto y_start = (int)(offsets[b][n][1]);
     auto y_end = y_start + patch_W;
 
-    for (auto i = x_start; i < x_end; i++)
-    {
-        for (auto j = y_start; j < y_end; j++)
-        {
-            patches[patch_idx][c][i][j] += img[b][c][i][j];
-        }
-    }
+    for (auto img_i = x_start, patch_i = 0; img_i < x_end; img_i++, patch_i++)
+        for (auto img_j = y_start, patch_j = 0; img_j < y_end; img_j++, patch_j++)
+            patches[patch_idx][c][patch_i][patch_j] = img[b][c][img_i][img_j];
 }
 
 torch::Tensor extract_patches_cuda(torch::Tensor img, torch::Tensor offsets, c10::ArrayRef<long int> patch_size)
 {
     img = img.permute({0, 3, 1, 2});
+
+    auto batch_size = img.size(0);
+    auto channels = img.size(1);
+    auto n_samples = offsets.size(1);
+
+    printf("%d, %d, %d\n", batch_size, channels, n_samples);
 
     auto pad_const = (int)(patch_size[0] / 2.0);
     img = torch::constant_pad_nd(img, {pad_const, pad_const, pad_const, pad_const}, 0.0);
@@ -51,25 +55,21 @@ torch::Tensor extract_patches_cuda(torch::Tensor img, torch::Tensor offsets, c10
     auto patch_H = patch_size[0];
     auto patch_W = patch_size[1];
 
-    auto offset_acc = offsets.accessor<float, 3>();
+    auto options = torch::TensorOptions().device(torch::kCUDA, 0);
 
-    auto batch_size = img.size(0);
-    auto channels = img.size(1);
-    auto n_samples = offsets.size(1);
-
-    auto patches = torch::zeros({batch_size * n_samples, channels, patch_H, patch_W});
+    auto patches = torch::zeros({batch_size * n_samples, channels, patch_H, patch_W}, options);
 
     const int n = batch_size * n_samples * channels;
-    const int threads = 1024;
+    const int threads = 64;
     const int blocks = (n + threads - 1) / threads;
 
-    AT_DISPATCH_FLOATING_TYPES(img.type(), "extract_patch", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(img.scalar_type(), "extract_patch", ([&] {
                                    extract_patches_kernel<scalar_t><<<blocks, threads>>>(
                                        img.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
                                        offsets.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>(),
-                                       batch_size, channels, n_samples, patch_H,
+                                       batch_size, n_samples, channels, patch_H,
                                        patch_W, patches.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>());
                                }));
 
-    return torch::stack(patches);
+    return patches;
 }
--- ats/ops/setup.py
+++ ats/ops/setup.py
@@ -1,10 +1,17 @@
-from setuptools import Extension, setup
-from torch.utils import cpp_extension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name="extract_patches",
+    name="ops",
     ext_modules=[
-        cpp_extension.CppExtension("extract_patches", ["extract_patches.cpp"])
+        CUDAExtension(
+            name="extract_patches",
+            sources=[
+                "extract_patches.cpp",
+                "extract_patches_kernel.cu",
+            ],
+            extra_compile_args={"nvcc": ["-g", "-G"], "cxx": ["-g"]},
+        )
     ],
-    cmdclass={"build_ext": cpp_extension.BuildExtension},
+    cmdclass={"build_ext": BuildExtension},
 )
