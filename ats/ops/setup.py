ba399dc850183d8a0033effe0eed310d383e3c08
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-03-18 12:30:52 +0000

    Add optinal debug mode to setup.py

--- ats/ops/setup.py
+++ ats/ops/setup.py
@@ -1,5 +1,12 @@
+import os
+
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+DEBUG = os.environ.get("ADD_DEBUG_FLAGS", "False") == "True"
+
+extra_compile_args = {"nvcc": ["-g", "-G"], "cxx": ["-g"]} if DEBUG else {}
+
 
 setup(
     name="ops",
@@ -10,7 +17,7 @@ setup(
                 "extract_patches.cpp",
                 "extract_patches_kernel.cu",
             ],
-            extra_compile_args={"nvcc": ["-g", "-G"], "cxx": ["-g"]},
+            extra_compile_args=extra_compile_args,
         )
     ],
     cmdclass={"build_ext": BuildExtension},
