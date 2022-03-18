eca2fa799c162aff455f122af438cbb472682e9e
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-03-18 16:36:11 +0000

    Change test to use installed module

--- tests/extract_patches/test_extract_patches.py
+++ tests/extract_patches/test_extract_patches.py
@@ -1,6 +1,6 @@
 import torch
 
-import ops  # isort:skip
+from ats import ops  # isort:skip
 
 
 img = torch.rand(16, 4000, 4000, 3, dtype=torch.float32, device="cuda")
@@ -8,6 +8,6 @@ offsets = torch.zeros(16, 10, 2, dtype=torch.float32, device="cuda")
 
 print(img)
 
-patches = ops.extract_patches(img, offsets, (6, 6))
+patches = ops.extract_patches(img, offsets, (256, 256))
 
 print(patches)
